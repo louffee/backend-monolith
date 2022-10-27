59c184aa5ca27185d587720ada5dd1830957b8c7
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-10-27 19:47:00 -0300

    refactor: create a const value to store the limit of exchange between server and client

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -1,9 +1,11 @@
 import { v4 } from 'uuid'
 
 const AUTH_TOKEN_SECRET = v4().replace(/-/g, '')
+const EXTERNAL_DATA_TRANSFER_LIMIT = '50mb'
 
 const constants = {
   AUTH_TOKEN_SECRET,
+  EXTERNAL_DATA_TRANSFER_LIMIT,
 }
 
 export default constants
