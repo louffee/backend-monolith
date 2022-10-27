3544d796afb41ea9b2092c46f0dfe417244f2a0f
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-10-27 18:48:08 -0300

    chore: disable jest/no-jest-import rule in ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,10 @@
-module.exports = require('eslint-config-louffee/_base')({ isBrowser: false })
+var config = require('eslint-config-louffee/_base')({ isBrowser: false })
+
+module.exports = {
+  ...config,
+  rules: {
+    ...config.rules,
+    'promise/no-native': 'off',
+    'jest/no-jest-import': 'off',
+  },
+}
