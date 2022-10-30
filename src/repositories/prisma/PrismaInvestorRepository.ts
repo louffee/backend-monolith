9eecd712da0ff3c0c515ed11d02a559f9e10efc7
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-10-29 23:57:08 -0300

    refactor(repositories): add the reserved keyword 'public' at the beginning of the methods

--- src/repositories/prisma/PrismaInvestorRepository.ts
+++ src/repositories/prisma/PrismaInvestorRepository.ts
@@ -11,27 +11,27 @@ class PrismaInvestorRepository implements InvestorRepository {
     this.client = new PrismaClient()
   }
 
-  async createInvestor(investor: InvestorCreateDataTransferObject): Promise<Investor> {
+  public async createInvestor(investor: InvestorCreateDataTransferObject): Promise<Investor> {
     return await this.client.investor.create({ data: { ...investor, createdAt: new Date(), updatedAt: new Date() } })
   }
 
-  async findByIdInvestor(id: string): Promise<Investor | null> {
+  public async findByIdInvestor(id: string): Promise<Investor | null> {
     return await this.client.investor.findFirst({ where: { id } })
   }
 
-  async findByEmailInvestor(email: string): Promise<Investor | null> {
+  public async findByEmailInvestor(email: string): Promise<Investor | null> {
     return await this.client.investor.findFirst({ where: { InvestorContact: { some: { email } } } })
   }
 
-  async fetchAllInvestors(): Promise<Investor[]> {
+  public async fetchAllInvestors(): Promise<Investor[]> {
     return await this.client.investor.findMany()
   }
 
-  async fetchAllPaginatedInvestors(limit: number, offset: number): Promise<Investor[]> {
+  public async fetchAllPaginatedInvestors(limit: number, offset: number): Promise<Investor[]> {
     return await this.client.investor.findMany({ take: limit, skip: offset })
   }
 
-  async updateInvestor({ id, ...investor }: Investor): Promise<void> {
+  public async updateInvestor({ id, ...investor }: Investor): Promise<void> {
     await this.client.investor.update({
       where: { id },
       data: { ...investor, updatedAt: new Date() },
