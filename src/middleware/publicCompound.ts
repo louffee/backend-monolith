45685b252a3d66617dce0fb1568c315c7b503193
Leonardo Lemos <leo.lemos.ds@icloud.com>
2022-10-30 17:12:21 -0300

    refactor: only mention the folder name and set node to handle the static files pwd

--- src/middleware/publicCompound.ts
+++ src/middleware/publicCompound.ts
@@ -1,17 +1,7 @@
-import path from 'node:path'
-import url from 'node:url'
-
 import express from 'express'
 
 function publicCompound() {
-  // eslint-disable-next-line no-undef
-  const pwd = process.cwd()
-  const dirname = path.dirname(url.fileURLToPath(pwd))
-  const publicFolderName = 'public'
-
-  const staticDirectory = path.join(dirname, publicFolderName)
-
-  return express.static(staticDirectory)
+  return express.static('public')
 }
 
 export default publicCompound
